e1f0b0af49d3bb1e134970755b195d6412c9d6d0
Behnam <behnam.sep@gmail.com>
2024-08-23 13:30:21 +0200

    Init Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./app.scss";
+const App = () => {
+  return (
+    <div>
+      <section><a href="">Hero</a></section>
+      <section>Prallex</section>
+      <section>Services</section>
+      <section>Prallex</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </div>
+  );
+};
+
+export default App;
